966d0d00867a58bd604b43f58dcd747d2abba5d2
Eric Van Hensbergen <ericvh@kernel.org>
2024-05-14 20:00:48 +0000

    Remove hole punch tests
    This created issues on github runner environment
    
    Signed-off-by: Eric Van Hensbergen <ericvh@kernel.org>

--- tests/ci/fsx.bash
+++ tests/ci/fsx.bash
@@ -5,7 +5,7 @@
 #fsx/fsx -N 1000 -R $PATH_MNTDIR/testfile
 
 # passes with mmap disabled
-if $TESTBIN/fsx/fsx -N 1000 -R -W $PATH_MNTDIR/testfile > $LOG.log 2>&1; then
+if $TESTBIN/fsx/fsx -N 1000 -R -W -H $PATH_MNTDIR/testfile > $LOG.log 2>&1; then
     echo "SUCCESS"
 else
     echo "FAIL"
