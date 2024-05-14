f4cf8e709436b0214e1d50911784b98db2d629c2
Eric Van Hensbergen <ericvh@kernel.org>
2024-05-14 20:21:12 +0000

    Remove pre-existing files to eliminate potential corruption
    Signed-off-by: Eric Van Hensbergen <ericvh@kernel.org>

--- tests/ci/fsx-mmap.bash
+++ tests/ci/fsx-mmap.bash
@@ -4,6 +4,8 @@
 #fsx/fsx -N 1000 -W $PATH_MNTDIR/testfile
 #fsx/fsx -N 1000 -R $PATH_MNTDIR/testfile
 
+rm -rf $PATH_MNTDIR/testfile
+
 # passes with mmap disabled
 if $TESTBIN/fsx/fsx -N 1000 $PATH_MNTDIR/testfile > $LOG.log 2>&1; then
     echo "SUCCESS"
